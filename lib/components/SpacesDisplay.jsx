37f7d8b607af4dd3558a041e06767d035b5e4f00
Jean <jean.tinland@valraiso.fr>
2020-05-13 12:15:55 +0200

    SpacesDisplay component refacto

--- lib/components/SpacesDisplay.jsx
+++ lib/components/SpacesDisplay.jsx
@@ -2,23 +2,19 @@ export const refreshFrequency = false;
 
 const SpacesDisplay = ({ output }) => {
   const { displays, spaces } = output;
-  if (!output) {
-    return <div className="simple-bar simple-bar--empty"></div>;
-  }
-  return displays.map((display, i) => {
-    return (
-      <div key={i} className="spaces-display">
-        {spaces.map((space, i) => {
-          const classes = space.focused ? 'space space--focused' : 'space';
-          return display.index === space.display ? (
-            <span key={i} className={classes}>
-              {space.index}
-            </span>
-          ) : null;
-        })}
-      </div>
-    );
-  });
+  if (!output) return <div className="spaces-display spaces-display--empty" />;
+  return displays.map((display, i) => (
+    <div key={i} className="spaces-display">
+      {spaces.map((space, i) => {
+        const classes = space.focused ? 'space space--focused' : 'space';
+        return display.index === space.display ? (
+          <span key={i} className={classes}>
+            {space.index}
+          </span>
+        ) : null;
+      })}
+    </div>
+  ));
 };
 
 export default SpacesDisplay;
