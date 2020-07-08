2d4cbdbd111913ca531c420993deabdcebe6ba8d
Amar1729 <amar.paul16@gmail.com>
2020-07-08 18:09:22 -0400

    make wifi div clickable (enables/disables wifi)

--- lib/components/Wifi.jsx
+++ lib/components/Wifi.jsx
@@ -1,16 +1,29 @@
 import { Wifi, WifiOff } from './Icons.jsx';
+import { run } from "uebersicht";
+
+const wifi_on = () => {
+  run(`osascript -e 'tell app "System Events" to display notification "enabling wifi." with title "simple-bar"'`);
+  run(`networksetup -setairportpower en0 on`);
+};
+
+const wifi_off = () => {
+  run(`osascript -e 'tell app "System Events" to display notification "disabling wifi." with title "simple-bar"'`);
+  run(`networksetup -setairportpower en0 off`);
+};
 
 const render = ({ output }) => {
   if (!output) return null;
   const { status, ssid } = output;
   const isActive = status === 'active';
 
-  const classes = isActive ? 'wifi' : 'wifi wifi--inactive';
+  const classes = 'clickable' + isActive ? 'wifi' : 'wifi wifi--inactive';
 
   const Icon = isActive ? Wifi : WifiOff;
 
+  const wifi_exec = isActive ? wifi_off : wifi_on;
+
   return (
-    <div className={classes}>
+    <div className={classes} onClick={wifi_exec}>
       <Icon className="wifi__icon" />
       {ssid && ssid}
     </div>
