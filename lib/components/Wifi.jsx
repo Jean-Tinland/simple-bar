5f427c1db4a2ae4929d3184e6fcad497f474085e
Jean <jean.tinland@gmail.com>
2020-07-09 13:46:07 +0200

    Amar Paul PR 2 optimization

--- lib/components/Wifi.jsx
+++ lib/components/Wifi.jsx
@@ -1,33 +1,45 @@
-import { Wifi, WifiOff } from './Icons.jsx';
-import { run } from "uebersicht";
+import { run } from 'uebersicht'
+import { Wifi, WifiOff } from './Icons.jsx'
 
-const wifi_on = () => {
-  run(`osascript -e 'tell app "System Events" to display notification "enabling wifi." with title "simple-bar"'`);
-  run(`networksetup -setairportpower en0 on`);
-};
+const refreshData = () =>
+  run(`osascript -e 'tell application id "tracesOf.Uebersicht" to refresh widget id "simple-bar-data-jsx"'`)
 
-const wifi_off = () => {
-  run(`osascript -e 'tell app "System Events" to display notification "disabling wifi." with title "simple-bar"'`);
-  run(`networksetup -setairportpower en0 off`);
-};
+const toggleWifi = (isActive) => {
+  if (isActive) {
+    run(`networksetup -setairportpower en0 off`)
+    run(`osascript -e 'tell app "System Events" to display notification "disabling wifi." with title "simple-bar"'`)
+  } else {
+    run(`networksetup -setairportpower en0 on`)
+    run(`osascript -e 'tell app "System Events" to display notification "enabling wifi." with title "simple-bar"'`)
+  }
+  refreshData()
+}
+
+const renderName = (name) => {
+  if (!name) return ''
+  if (name === 'with an AirPort network. y off.') return 'Disabled'
+  if (name === 'with an AirPort network.') return 'Searching...'
+  return name
+}
 
 const render = ({ output }) => {
-  if (!output) return null;
-  const { status, ssid } = output;
-  const isActive = status === 'active';
+  if (!output) return null
+  const { status, ssid } = output
+  const isActive = status === 'active'
+  const name = renderName(ssid)
 
-  const classes = 'clickable' + isActive ? 'wifi' : 'wifi wifi--inactive';
+  const classes = isActive ? 'wifi' : 'wifi wifi--inactive'
 
-  const Icon = isActive ? Wifi : WifiOff;
+  const Icon = isActive ? Wifi : WifiOff
 
-  const wifi_exec = isActive ? wifi_off : wifi_on;
+  const clicked = () => toggleWifi(isActive)
 
   return (
-    <div className={classes} onClick={wifi_exec}>
+    <div className={classes} onClick={clicked}>
       <Icon className="wifi__icon" />
-      {ssid && ssid}
+      {name}
     </div>
-  );
-};
+  )
+}
 
-export default render;
+export default render
