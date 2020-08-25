3cd00230bf9d1d20b5492401bd9d8ec5f82987a4
Jean <jean.tinland@gmail.com>
2020-08-25 17:13:59 +0200

    Update battery indicator color while charging & on low battery

--- lib/components/Battery.jsx
+++ lib/components/Battery.jsx
@@ -11,7 +11,7 @@ const Battery = ({ output }) => {
   if (!output) return
   const { percentage, charging /*, remaining */ } = output
   const isCharging = charging === 'true'
-  const isLowBattery = !isCharging && percentage < 10
+  const isLowBattery = !isCharging && percentage < 20
 
   const classes = isLowBattery ? 'battery battery--low' : 'battery'
 
