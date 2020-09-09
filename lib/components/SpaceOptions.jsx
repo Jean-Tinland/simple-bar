ede693d8d640943c567a0161b1bd8645db38162c
Jean <jean.tinland@gmail.com>
2020-09-09 13:09:34 +0200

    Animate space creation & removel

--- lib/components/SpaceOptions.jsx
+++ lib/components/SpaceOptions.jsx
@@ -9,6 +9,11 @@ const SpaceOptions = ({ index, displayId }) => {
     clickEffect(e)
     const target = e.target.closest('.space')
     target.classList.remove('space--hovered')
+    target.classList.add('space--removing')
+    setTimeout(() => {
+      if (!target) return
+      target.style.display = 'none'
+    }, 320)
     removeSpace(index, displayId)
   }
   const onChevronClick = (direction) => (e) => {
