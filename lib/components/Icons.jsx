4046b6d6b8618159abfb0e702d302699dfea71f6
Jamie Weavis <jamie.weavis@gmail.com>
2020-09-11 16:03:42 +0100

    Add separate date icon for Date component

--- lib/components/Icons.jsx
+++ lib/components/Icons.jsx
@@ -46,6 +46,12 @@ export const VolumeMuted = (props) => (
   </Icon>
 )
 
+export const Date = (props) => (
+  <Icon {...props}>
+    <path d="M21 3h-3V1h-2v2H8V1H6v2H3v18h18V3zm-2 16H5V8h14v11zM7 10h5v5H7v-5z" />
+  </Icon>
+)
+
 export const Calendar = (props) => (
   <Icon {...props}>
     <path d="M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zm-12 6h-4v4h4v-4zm6 0h-4v4h4v-4zm-6-6h-4v4h4v-4zm16-8v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2z" />
