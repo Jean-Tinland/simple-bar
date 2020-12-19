4a2d80cf252c86eb1663254607a0c5cb61b6c76e
AlexNaga <alex.naga@hotmail.com>
2020-12-19 18:11:45 +0100

    feat: open calendar app when clicking on date

--- lib/components/Date.jsx
+++ lib/components/Date.jsx
@@ -1,6 +1,13 @@
+import { run } from 'uebersicht'
+
 import { DateIcon } from './Icons.jsx'
+import { clickEffect } from '../utils.js'
 
 import { getSettings } from '../settings.js'
+
+const openCalendarApp = () => {
+  run('open -a Calendar')
+}
 
 const DateDisplay = () => {
   const settings = getSettings()
@@ -17,10 +24,15 @@ const DateDisplay = () => {
     day: 'numeric'
   }
 
+  const onClick = (e) => {
+    clickEffect(e)
+    openCalendarApp()
+  }
+
   const _locale = locale.length >= 5 ? locale : 'en-UK'
   const now = new Date().toLocaleDateString(_locale, options)
   return (
-    <div className="date">
+    <div className="date" onClick={onClick}>
       <DateIcon className="date__icon" />
       {now}
     </div>
