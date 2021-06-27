e67ae97f7f832d589e6ff2c270601c2bfe643326
Jean Tinland <jean.tinland@valraiso.fr>
2021-06-27 11:12:39 +0200

    All spaces can now be renamed by clicking on label with alt/option key pressed

--- lib/components/spaces/space.jsx
+++ lib/components/spaces/space.jsx
@@ -2,37 +2,55 @@ import { React } from 'uebersicht'
 import OpenedApps from './opened-apps.jsx'
 import SpaceOptions from './space-options.jsx'
 import { classnames, clickEffect, filterApps } from '../../utils'
-import { goToSpace } from '../../yabai'
+import { goToSpace, renameSpace } from '../../yabai'
 import { getSettings } from '../../settings'
 
-const { useState } = React
+const { useRef, useState } = React
 
 const settings = getSettings()
 
 const Space = ({ space, display, windows, displayIndex, SIPDisabled, lastOfSpace }) => {
+  const labelRef = useRef()
   const [hovered, setHovered] = useState(false)
   const [noDelay, setNoDelay] = useState(false)
+  const [editable, setEditable] = useState(false)
+  const { index, label, focused, visible, type } = space
+  const [spaceLabel, setSpaceLabel] = useState(label?.length ? label : index)
 
   const { spacesDisplay } = settings
   const { displayAllSpacesOnAllScreens, exclusionsAsRegex } = spacesDisplay
   if (!displayAllSpacesOnAllScreens && display !== space.display) return null
 
-  const { index, label, focused, visible, type } = space
   const exclusions = exclusionsAsRegex ? spacesDisplay.exclusions : spacesDisplay.exclusions.split(', ')
   const titleExclusions = exclusionsAsRegex ? spacesDisplay.titleExclusions : spacesDisplay.titleExclusions.split(', ')
 
   const onMouseEnter = (e) => {
+    const { altKey, metaKey } = e
+    if (altKey) return
     setHovered(true)
-    if (e.metaKey) setNoDelay(true)
+    if (metaKey) setNoDelay(true)
   }
   const onMouseLeave = () => {
     setHovered(false)
     setNoDelay(false)
+    setEditable(false)
+    window.getSelection().removeAllRanges()
   }
   const onClick = (e) => {
-    if (focused === 1) return
-    goToSpace(index)
-    clickEffect(e)
+    if (e.altKey) {
+      setEditable(true)
+      labelRef.current?.select()
+    } else {
+      if (focused === 1) return
+      goToSpace(index)
+      clickEffect(e)
+    }
+  }
+  const onChange = (e) => {
+    if (!editable) return
+    const newLabel = e.target.value
+    setSpaceLabel(newLabel)
+    renameSpace(index, newLabel)
   }
 
   const apps = windows.filter(
@@ -46,17 +64,27 @@ const Space = ({ space, display, windows, displayIndex, SIPDisabled, lastOfSpace
     'space--visible': visible === 1,
     'space--hovered': hovered,
     'space--no-delay': noDelay,
-    'space--empty': apps.length === 0
+    'space--empty': apps.length === 0,
+    'space--editable': editable
   })
 
-  const spaceLabel = label && label !== '' ? label : index
+  const labelSize = typeof spaceLabel === 'number' ? spaceLabel.toString().length : spaceLabel.length
 
   return (
     <>
       {spacesDisplay.displayAllSpacesOnAllScreens && lastOfSpace && <div className="spaces__separator" />}
       <div className={classes} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
         <button className="space__inner" onClick={onClick}>
-          <span className="space__label">{spaceLabel}</span> <OpenedApps type={type} apps={apps} />
+          <input
+            ref={labelRef}
+            type="text"
+            className="space__label"
+            onChange={onChange}
+            value={spaceLabel}
+            size={labelSize}
+            readOnly={!editable}
+          />
+          <OpenedApps type={type} apps={apps} />
         </button>
         {!spacesDisplay.hideSpacesOptions && SIPDisabled && (
           <SpaceOptions index={index} setHovered={setHovered} displayIndex={displayIndex} />
