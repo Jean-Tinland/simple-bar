eb6781cb2fe0e31dbcba8047e154c21589c52632
Kevin Dreßler <kvndrsslr@gmail.com>
2021-08-10 21:05:25 +0200

    sticky window workaround + new setting for hiding duplicate app icons in spaces display

---
+++ lib/components/spaces/stickies.jsx
@@ -0,0 +1,29 @@
+import * as Uebersicht from 'uebersicht'
+import OpenedApps from './opened-apps.jsx'
+import * as Utils from '../../utils'
+import * as Settings from '../../settings'
+
+
+const Stickies = ({ display, windows }) => {
+  const { spacesDisplay } = Settings.get()
+  const { exclusionsAsRegex, hideDuplicateAppsInSpaces } = spacesDisplay
+  const exclusions = exclusionsAsRegex ? spacesDisplay.exclusions : spacesDisplay.exclusions.split(', ')
+  const titleExclusions = exclusionsAsRegex ? spacesDisplay.titleExclusions : spacesDisplay.titleExclusions.split(', ')
+  
+  const {stickyWindows: apps} = Utils.stickyWindowWorkaround(windows, hideDuplicateAppsInSpaces, display, undefined, exclusions, titleExclusions, exclusionsAsRegex)
+
+  if (apps.length === 0) return null
+
+  return (
+    <Uebersicht.React.Fragment>
+      <div className="stickies">
+      <button className="stickies_inner">
+          <OpenedApps type="stickies" apps={apps} />
+      </button>
+      </div>
+      <div className="spaces__separator" />
+    </Uebersicht.React.Fragment>
+  )
+}
+
+export default Stickies
