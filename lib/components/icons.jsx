fd5d0c4bd48e82ee0a23fff68b5a0d362cabeaae
Kevin Dreßler <kvndrsslr@gmail.com>
2021-08-12 20:11:52 +0200

    add mpv icon

--- lib/components/icons.jsx
+++ lib/components/icons.jsx
@@ -906,6 +906,21 @@ export const ColorPicker = (props) => (
   </Icon>
 )
 
+export const Mpv = (props) => (
+  <Icon {...props}>
+  <g fill-rule="evenodd" clip-path="url(#clip0)" clip-rule="evenodd">
+    <path d="M24 12a12 12 0 11-24 0 12 12 0 0124 0zm-2.46-1.1a8.59 8.59 0 11-17.17 0 8.59 8.59 0 0117.17 0z"/>
+    <path fill-opacity=".6" d="M23.45 11.63a11.14 11.14 0 11-22.28 0 11.14 11.14 0 0122.28 0zm-6.51 2.53a5.52 5.52 0 10-10.2-4.22 5.52 5.52 0 0010.2 4.22z"/>
+    <path d="M10 9v6l5-3.07L10 9z"/>
+  </g>
+  <defs>
+    <clipPath id="clip0">
+      <path fill="#fff" d="M0 0h24v24H0z"/>
+    </clipPath>
+  </defs>
+ </Icon>
+)
+
 export const Default = (props) => (
   <Icon {...props}>
     <path
