64b0856ef446cd51f142d0d9ac16f2e5cde2f9e1
Omar Essam <omaressam@Omars-MacBook-Air.local>
2021-10-07 19:08:35 +0100

    Add icons for number labels

--- lib/components/icons.jsx
+++ lib/components/icons.jsx
@@ -354,6 +354,71 @@ export const GoogleChrome = (props) => (
   </Icon>
 )
 
+export const One = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Two = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Three = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Four = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Five = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Six = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Seven = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Eight = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Nine = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Ten = (props) => (
+  <Icon  {...props}>
+    <text y="50%" font-size="35" dominant-baseline="middle"></text>    
+  </Icon>
+)
+
+export const Empty = (props) => (
+  <Icon  {...props}>
+    <text x="50%" y="50%" font-size="25" dominant-baseline="middle" text-anchor="middle"></text>    
+  </Icon>
+)
 export const Skype = (props) => (
   <Icon {...props}>
     <path d="M23.3 14.55C25.06 6.74 18.2-.35 10.18 1 5.81-1.7 0 1.36 0 6.55c0 1.2.33 2.33.91 3.3C-.7 17.69 6.2 24.69 14.22 23.23c.94.5 2 .78 3.14.78a6.55 6.55 0 0 0 5.95-9.45zm-11.2 5.33c-2.35 0-4.63-.57-5.99-2.5-2.02-2.85.64-4.28 1.95-3.2 1.1.93.75 3.15 3.95 3.15 1.41 0 3.15-.6 3.15-2 0-2.82-6.24-1.49-8.73-4.55a3.77 3.77 0 0 1 .04-4.55c1.95-2.51 7.68-2.67 10.14-.77 2.28 1.76 1.66 4.1-.17 4.1-2.2 0-1.05-2.89-4.61-2.89-2.58 0-3.6 1.84-1.78 2.73 2.42 1.2 8.75.83 8.75 5.6 0 3.36-3.25 4.88-6.7 4.88z" />
