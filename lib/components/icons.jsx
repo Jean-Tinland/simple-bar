6456dd255d05398c14e700ff57b124c8e95e8060
badlydrawnface <bdface@protonmail.com>
2022-05-15 23:43:43 -0400

    added Parallels

--- lib/components/icons.jsx
+++ lib/components/icons.jsx
@@ -1321,6 +1321,12 @@ export const LibreWolf = (props) => (
   </Icon>
 )
 
+export const Parallels = (props) => (
+  <Icon {...props}>
+    <path fill-rule="evenodd" d="M8.002 0C6.902 0 6 .882 6 1.957v20.086C6 23.119 6.901 24 8.002 24c1.1 0 2.003-.881 2.003-1.957V1.957C10.005.882 9.103 0 8.002 0Zm7.996 0c-1.101 0-2.003.882-2.003 1.957v20.086c0 1.076.902 1.957 2.003 1.957 1.1 0 2.002-.881 2.002-1.957V1.957C18 .882 17.098 0 15.998 0Z" clip-rule="evenodd"/>
+  </Icon>
+)
+
 export const Default = (props) => (
   <Icon {...props}>
     <path
