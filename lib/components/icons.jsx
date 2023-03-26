e3b1888a7895373ac9ed7c33829fb96c7c3df118
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2023-03-27 10:45:09 +1100

    Update play/pause icons #332
    The standard is that the paused icon is the right pointing arrow, and the two vertical lines is paused. This will update the icons to follow these standards.

--- lib/components/icons.jsx
+++ lib/components/icons.jsx
@@ -201,13 +201,13 @@ export const Date = (props) => (
   </Icon>
 );
 
-export const Playing = (props) => (
+export const Paused = (props) => (
   <Icon {...props}>
     <path d="M4.8 20V4l14.4 8-14.4 8z" />
   </Icon>
 );
 
-export const Paused = (props) => (
+export const Playing = (props) => (
   <Icon {...props}>
     <path d="M5.7 4h3.8c.5 0 .9.4.9.9v14.2c0 .5-.4.9-1 .9H5.8a1 1 0 01-.9-.9V4.9c0-.5.4-.9 1-.9zm8.8 0h3.8c.5 0 .9.4.9.9v14.2c0 .5-.4.9-1 .9h-3.7a1 1 0 01-.9-.9V4.9c0-.5.4-.9 1-.9z" />
   </Icon>
