1e151f95a0e0fba18e35dffa76acb7fc60ea432a
Jean Tinland <jean.tinland@gmail.com>
2023-12-23 16:03:39 +0100

    Cleanup AnyType paths + run prettier in app-icons

--- lib/components/icons.jsx
+++ lib/components/icons.jsx
@@ -1539,8 +1539,7 @@ export const TablePlus = (props) => (
 
 export const Anytype = (props) => (
   <Icon {...props}>
-    <path d="M3.5 0h14.4v5.2H3.5zm14.4 5.2h5.6v18.7h-5.6z"/>
-    <circle cx="8" cy="16.5" r="7.5"/>
+    <path d="M3.6304 0h15.0261v5.2H3.6305V0Zm15.0261 5.2H24.5v18.7h-5.8435V5.2ZM8.326 24c4.3223 0 7.8262-3.3579 7.8262-7.5S12.6483 9 8.3261 9C4.0039 9 .5 12.3579.5 16.5S4.0039 24 8.326 24Z" />
   </Icon>
 );
 
