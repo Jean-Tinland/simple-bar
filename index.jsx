10c9d4200fffb6f3f49fc0ec871f0429ddd6aced
Jean Tinland <jean.tinland@gmail.com>
2023-12-23 16:09:23 +0100

    Simplify data widget section hiding if empty with CSS only

--- index.jsx
+++ index.jsx
@@ -34,9 +34,6 @@ const refreshFrequency = false;
 const settings = Settings.get();
 const { yabaiPath = "/usr/local/bin/yabai", shell } = settings.global;
 const { processWidget } = settings.widgets;
-const hasAnyWidgetEnabled = Object.entries(settings.widgets)
-  .filter(([widgetName]) => widgetName !== "processWidget")
-  .some(([_, enabled]) => enabled);
 const { hideWindowTitle, displayOnlyIcon, displaySkhdMode } = settings.process;
 
 const enableTitleChangedSignal = hideWindowTitle || displayOnlyIcon;
@@ -131,29 +128,27 @@ const render = ({ output, error }) => {
         />
       )}
       <Settings.Wrapper />
-      {hasAnyWidgetEnabled && (
-        <div className="simple-bar__data">
-          <UserWidgets display={displayIndex} />
-          <Zoom.Widget display={displayIndex} />
-          <BrowserTrack.Widget display={displayIndex} />
-          <Spotify.Widget display={displayIndex} />
-          <Crypto.Widget display={displayIndex} />
-          <Stock.Widget display={displayIndex} />
-          <Music.Widget display={displayIndex} />
-          <Mpd.Widget display={displayIndex} />
-          <Weather.Widget display={displayIndex} />
-          <Netstats.Widget display={displayIndex} />
-          <Cpu.Widget display={displayIndex} />
-          <Battery.Widget display={displayIndex} />
-          <Mic.Widget display={displayIndex} />
-          <Sound.Widget display={displayIndex} />
-          <ViscosityVPN.Widget display={displayIndex} />
-          <Wifi.Widget display={displayIndex} />
-          <Keyboard.Widget display={displayIndex} />
-          <DateDisplay.Widget display={displayIndex} />
-          <Time.Widget display={displayIndex} />
-        </div>
-      )}
+      <div className="simple-bar__data">
+        <UserWidgets display={displayIndex} />
+        <Zoom.Widget display={displayIndex} />
+        <BrowserTrack.Widget display={displayIndex} />
+        <Spotify.Widget display={displayIndex} />
+        <Crypto.Widget display={displayIndex} />
+        <Stock.Widget display={displayIndex} />
+        <Music.Widget display={displayIndex} />
+        <Mpd.Widget display={displayIndex} />
+        <Weather.Widget display={displayIndex} />
+        <Netstats.Widget display={displayIndex} />
+        <Cpu.Widget display={displayIndex} />
+        <Battery.Widget display={displayIndex} />
+        <Mic.Widget display={displayIndex} />
+        <Sound.Widget display={displayIndex} />
+        <ViscosityVPN.Widget display={displayIndex} />
+        <Wifi.Widget display={displayIndex} />
+        <Keyboard.Widget display={displayIndex} />
+        <DateDisplay.Widget display={displayIndex} />
+        <Time.Widget display={displayIndex} />
+      </div>
     </div>
   );
 };
