478d5c77a7a97dd6089efc0609736efa398fbe33
haxy <zaidsaheb4@gmail.com>
2024-07-17 11:37:49 +0300

    icon appears

--- index.jsx
+++ index.jsx
@@ -30,7 +30,7 @@ import * as DataWidgetLoader from "./lib/components/data/data-widget-loader.jsx"
 import * as DataWidget from "./lib/components/data/data-widget.jsx";
 import * as Utils from "./lib/utils";
 import * as Settings from "./lib/settings";
-import MyIcon from "./lib/components/sideicon.jsx";
+import SideIcon from "./lib/components/side-icon.jsx";
 
 const refreshFrequency = false;
 
@@ -129,7 +129,7 @@ function render({ output, error }) {
           windows={windows}
           skhdMode={skhdMode}
         >
-          <MyIcon />
+          <SideIcon />
           <Spaces.Component />
           <Process.Component />
         </YabaiContextProvider>
