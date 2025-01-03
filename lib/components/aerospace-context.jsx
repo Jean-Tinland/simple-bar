68539bdbb4eed25584b39640c0c80ddb85c34648
Jean Tinland <jean.tinland@gmail.com>
2025-01-03 11:56:29 +0100

    Add AeroSpace simple-bar-server support

--- lib/components/aerospace-context.jsx
+++ lib/components/aerospace-context.jsx
@@ -1,5 +1,6 @@
 import * as Uebersicht from "uebersicht";
 import { useSimpleBarContext } from "./simple-bar-context.jsx";
+import useServerSocket from "../hooks/use-server-socket.js";
 import * as Aerospace from "../aerospace.js";
 
 const { React } = Uebersicht;
@@ -17,42 +18,51 @@ export function useAerospaceContext() {
 export default React.memo(AerospaceContextProvider);
 
 function AerospaceContextProvider({ children }) {
-  const { displayIndex, displays } = useSimpleBarContext();
+  const { settings, displayIndex, displays } = useSimpleBarContext();
+  const { enableServer, aerospaceServerRefresh } = settings.global;
+  const serverEnabled = enableServer && aerospaceServerRefresh;
 
   const [aerospaceSpaces, setAerospaceSpaces] = React.useState([]);
 
+  const getSpaces = React.useCallback(async () => {
+    let focusedWindow = {};
+    const [focusedSpace] = await Aerospace.getFocusedSpace();
+    try {
+      [focusedWindow] = await Aerospace.getFocusedWindow();
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
+    const spaces = await Promise.all(
+      displays.map(async (display) => {
+        const result = await Aerospace.getSpaces(display["monitor-id"]);
+        return Promise.all(
+          result.map(async (space) => {
+            const focused = space.workspace === focusedSpace.workspace;
+            const windows = await Aerospace.getWindows(space.workspace);
+            const formatted = windows.map((window) => {
+              const focused =
+                window["window-id"] === focusedWindow["window-id"];
+              return {
+                ...window,
+                focused,
+              };
+            });
+            return { ...space, windows: formatted, focused };
+          })
+        );
+      })
+    );
+    setAerospaceSpaces(spaces.flat());
+  }, [displays]);
+
+  const resetSpaces = () => {
+    setAerospaceSpaces([]);
+  };
+
+  useServerSocket("spaces", serverEnabled, getSpaces, resetSpaces);
+
   React.useEffect(() => {
-    const updateState = async () => {
-      let focusedWindow = {};
-      const [focusedSpace] = await Aerospace.getFocusedSpace();
-      try {
-        [focusedWindow] = await Aerospace.getFocusedWindow();
-        // eslint-disable-next-line no-empty
-      } catch (e) {}
-      const spaces = await Promise.all(
-        displays.map(async (display) => {
-          const result = await Aerospace.getSpaces(display["monitor-id"]);
-          return Promise.all(
-            result.map(async (space) => {
-              const focused = space.workspace === focusedSpace.workspace;
-              const windows = await Aerospace.getWindows(space.workspace);
-              const formatted = windows.map((window) => {
-                const focused =
-                  window["window-id"] === focusedWindow["window-id"];
-                return {
-                  ...window,
-                  focused,
-                };
-              });
-              return { ...space, windows: formatted, focused };
-            })
-          );
-        })
-      );
-      setAerospaceSpaces(spaces.flat());
-    };
-    updateState();
-  }, [displayIndex, displays]);
+    getSpaces();
+  }, [getSpaces, displayIndex]);
 
   return (
     <AerospaceContext.Provider value={{ spaces: aerospaceSpaces }}>
